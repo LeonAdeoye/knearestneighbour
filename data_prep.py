b780e051c3d6028cbcfddb0b898216df71d25b6c
LeonAdeoye <leon.adeoye@gmail.com>
2021-05-19 20:42:34 +0800

    data prep start

--- data_prep.py
+++ data_prep.py
@@ -1,7 +1,15 @@
-def print_start():
-    print('preparing data...')  # Press ⌘F8 to toggle the breakpoint.
+import pandas as pd
+
+
+def read_data():
+    print(f'Reading CSV file: wdbc.data into dataframe with Panda {pd.__version__}')
+    # read a CSV file with a comma as a delimiter, assume no header, and skip the first column
+    bc_dataFrame = pd.read_csv("../data/wdbc.data", sep=',', header=None, usecols=list(range(1, 32)))
+    print(f"Head of data frame:\n{bc_dataFrame.head()}")
+    print(f"First row:\n {bc_dataFrame.loc[0]}")
+
 
 if __name__ == '__main__':
-    print_start('PyCharm')
+    read_data()
 
 
